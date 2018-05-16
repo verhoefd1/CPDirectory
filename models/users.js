9c60707866bff92a2445c93a37beda23c02e4e81
Dylan <verhoefd1@gmail.com>
2018-05-16 16:46:09 -0500

    Removed references to passport

--- models/users.js
+++ models/users.js
@@ -1,15 +1,15 @@
-var mongoose = require("mongoose");
-//allows the plugins to be used. 
-var passportLocalMongoose = require("passport-local-mongoose");
+// var mongoose = require("mongoose");
+// //allows the plugins to be used. 
+// var passportLocalMongoose = require("passport-local-mongoose");
 
 
-//sets up schema for users
-var UserSchema = new mongoose.Schema({
-    username: String,
-    password: String
-});
+// //sets up schema for users
+// var UserSchema = new mongoose.Schema({
+//     username: String,
+//     password: String
+// });
 
-//adds in methods for use on app.js to add functionality to the users
-UserSchema.plugin(passportLocalMongoose);
-//exports
-module.exports = mongoose.model("User", UserSchema);
+// //adds in methods for use on app.js to add functionality to the users
+// UserSchema.plugin(passportLocalMongoose);
+// //exports
+// module.exports = mongoose.model("User", UserSchema);
