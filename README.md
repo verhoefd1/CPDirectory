9bb635053404ea2c7433e7c5085c285e7692cd9c
Dylan Verhoef <verhoefd1@gmail.com>
2018-06-09 09:35:47 -0500

    Commit changes

--- README.md
+++ README.md
@@ -1,19 +1,7 @@
 # CPDirectory
-A scalable interface for the Colony Park Directory that will eventually be interactive - This was mostly just a personal project that I'll build on over time and a chance for me to get my feet wet in using Git and some of the other technologies I've been learning. I just finished deploying the initial version on the 21st of May 2018. Not sure how much more I'll do to it. I have several goals in mind for future versions but at the moment it's where I need it to be so we'll see. 
+A scalable interface for the Colony Park Directory that will eventually be interactive
 
-# Installation - Ubuntu - this is what I built it on then deployed it to a Raspberri Pi attached to a public TV to display tenant informaiton and locations as well as weather. The insructions are fairly similar for initial installation. Deployment instructions will assume it's on a Raspberri Pi. 
-
-Install MongoDB - Keep an eye on what version you end up with as it will change which Mongoose version you can use. On Raspbian which is what this was deployed on, the release for MongoDB was: 2.4.14 which means Mongoose had to be downgraded to 4.0. More on the version compatibility here: http://mongoosejs.com/docs/compatibility.html
-
-I recommend following the documentation for MongoDB installation in their docs to prevent issues. 
-
-sudo apt-get install MongoDB
-
-Test to make sure that MongoDB is up and running when it's done installing by type mongo into your terminal. It should start the MongoDB Shell and you can use commands like show dbs. There should only be one currently called Local. CTRL+C to exit the shell
-
-mongo
-
-You can find version by type mongodb --version
+# Installation - Ubuntu
 
 Install NPM
 
@@ -21,24 +9,8 @@ sudo apt install npm -y
 
 Install Packages
 
-sudo npm install express ejs connect-flash express-session express-sanitizer method-override mongoose passport passport-local passport-local-mongoose request forever nodemon -g --save
 
-Once that is finished installing, you should be able to run the app assuming your Mongoose version lines up with the MongoDB and find the page at localhost:8080
-
-Use this command to start the server (Make sure you're in the directory containing app.js)
-
-nodemon app.js - auto restarts after saves to server side files
-
-or sudo nodejs app.js - basic start
-
-or forever start app.js - will be used later to run upon startup of computer
-
-Test the connection to MongoDB by uncommenting seedDB(); and restarting the node server or by adding a tenant at localhost:8080/tenants and click add a tenant.
-
-
-
-
-
+sudo npm install express ejs connect-flash express-session express-sanitizer method-override mongoose passport passport-local passport-local-mongoose request -y
 
 
 
--- app.js
+++ app.js
@@ -12,23 +12,19 @@ seedDB              = require("./seeds"),
 request				= require("request"),
 port                = 8080;
 
-console.log(process.env.DATABASEURL);
 
 //These are the routes under routes folder. This is so that we can separate our routes for each portion into separate files so that  app.js isn't humungous.
-//for use with router
-// var tenantRoutes = require("./routes/tenants");
+
+var tenantRoutes = require("./routes/tenants");
 // seedDB();
 // This does something to add all the directories into a the express system so that oyu don't have to explicitly say which folder it is in. I think 
 app.use(express.static(__dirname + "/public"));
 //Method over ride allows us to use server commands that don't work in the current http environmnet. 
 app.use(methodOverride("_method"));
 //connects to database
-mongoose.connect(process.env.DATABASEURL);
-//mongoose.connect("mongodb://localhost/directory");
+mongoose.connect("mongodb://localhost/directory");
 //sets up parsing to read req.body
 app.use(bodyParser.urlencoded({extended: true}));
-//sets up static location for the views directory so that if the app is launched as a service 
-app.set("views", __dirname + "/views")
 //sets up no need for .ejs after filenames
 app.set("view engine", "ejs");
 //adds the middleware to all the routes without explicitly adding it. 
@@ -54,116 +50,11 @@ app.set("view engine", "ejs");
 //     // res.locals.success = req.flash("success");
 //     next();
 // });
-app.get("/", function(req, res){ 
-	var url = "http://api.openweathermap.org/data/2.5/weather?q=West+Des+Moines&units=imperial&appid=8deb9186ad62ba1d2113ab0ba186c3a1";
-	request(url, function(error, response, body){
-		if(!error && response.statusCode === 200){
-			var weatherData = JSON.parse(body);
-			console.log(weatherData.weather[0].id);
-			Tenant.find({}).sort('name').exec(function(err, allTenants){
-        		if(err){
-            	console.log(err);
-        		} else {
-            		res.render("tenants/index", {tenants: allTenants, currentUser: req.user, page: 'display', weather: weatherData});
-        		}
-    		});
-		} else {
-			console.log(err);
-		}
-	});
-});
 
-//Tenant Index area for admins add in middleware
-app.get("/tenants", function(req, res){
-	Tenant.find({}).sort('name').exec(function(err, allTenants){
-		if(err){
-			console.log(err);
-		} else {
-			res.render("tenants/adminindex", {tenants: allTenants, currentUser: req.user, page: 'adminindex'});
-		}
-	});
-});
-
-//New Tenants
-app.get("/tenants/new", function(req, res){
-	res.render("tenants/new", {page: 'new'});
-});
-
-//Create Tenants
-app.post("/tenants", function(req, res){
-	var name 		= req.body.name;
-	var suite 		= req.body.suite;
-	var column 		= req.body.column;
-	var subname1 	= req.body.subname1;
-	var subname2 	= req.body.subname2;
-	var subname3 	= req.body.subname3;
-	var subname4 	= req.body.subname4;
-	var subname5 	= req.body.subname5;
-	var subname6 	= req.body.subname6;
-	var subname7 	= req.body.subname7;
-	var subname8 	= req.body.subname8;
-	var subname9 	= req.body.subname9;
-	var subname10 	= req.body.subname10;
-	var newTenant 	= {name:name, suite:suite, column:column, subname1:subname1,
-		subname2:subname2, subname3:subname3, subname4:subname4, subname5:subname5, subname6:subname6,
-		subname7:subname7, subname8:subname8, subname9:subname9, subname10:subname10};
-	//New tenant logic to add into database	
-	Tenant.create(newTenant, function(err, newlyCreated){
-		if(err){
-			console.log(err);
-		} else {
-			console.log(newlyCreated);
-			res.redirect("/tenants");
-		}
-	});
-});
-//Show Tenants
-app.get("/tenants/:tenant_id", function(req, res){
-	Tenant.findById(req.params.tenant_id).exec(function(err, foundTenant){
-		if(err){
-			console.log(err);
-		} else {
-			console.log(foundTenant);
-			res.render("tenants/show", {tenant: foundTenant, page: 'show'});
-		}
-	});
-});
-//Edit Tenants
-app.get("/tenants/:tenant_id/edit", function(req, res){
-	Tenant.findById(req.params.tenant_id, function(err, foundTenant){
-		if(err){
-			console.log(err);
-		} else {
-			console.log(foundTenant);
-			res.render("tenants/edit", {tenant: foundTenant, page: 'edit'});
-		}
-	});
-});
-//Update Tenants
-app.put("/tenants/:tenant_id", function(req, res){
-	Tenant.findByIdAndUpdate(req.params.tenant_id, req.body.tenant, function(err){
-		if(err){
-			console.log(err);
-		} else {
-			res.redirect("/tenants/" + req.params.tenant_id);
-		}
-	});
-});
-//Delete Tenants
-app.delete("/tenants/:tenant_id", function(req, res){
-	Tenant.findByIdAndRemove(req.params.tenant_id, function(err){
-		if(err){
-			console.log(err);
-		} else {
-			res.redirect("/tenants");
-		}
-	});
-});
-
-//Sets up the actual routes for each of the index pages. for use with router 
-//app.use("/", tenantRoutes);
+//Sets up the actual routes for each of the index pages. 
+app.use("/", tenantRoutes);
 
 //sets up server to start with app.
 app.listen(port, function(){
-   console.log("Directory Server Started listing on " + port);
+   console.log("Directory Server Started listing on" + port);
 });
